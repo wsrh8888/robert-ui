b7dfdbaaf585e706f7a819e6a2ccde1aefba2067
任皓 <751135385@qq.com>
2020-03-22 20:11:14 +0800

    [fixed]webpack增加scss模块解析

--- examples/app.vue
+++ examples/app.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-      <rb-alert title="dddd"></rb-alert>
-       <router-view/>
+        <rb-alert title="dddd" class="ddd"></rb-alert>
+        <router-view />
     </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-  import Component from 'vue-class-component'
+import Vue from "vue";
+import Component from "vue-class-component";
 
-  @Component
-  export default class App extends Vue {
+@Component
+export default class App extends Vue {
     // 初始化数据
-    msg = 123
-
-  }
+    msg = 123;
+}
 </script>
+
