3b1d5191f3ca4f3725e71372fd2db2b2b27f9a83
任皓 <751135385@qq.com>
2020-04-03 20:11:46 +0800

    [feature]alert增加ul样式

--- packages/alert/src/main.vue
+++ packages/alert/src/main.vue
@@ -1,8 +1,19 @@
 <template>
-    <transition name="el-alert-fade">
-        <div class="rb-alert">
-            <i></i>
-            <slot>{{ title }}</slot>
+    <transition name="rb-alert-fade">
+        <div class="rb-alert"
+        :class="[typeClass,center ? 'is-center' : '', 'is-' + effect]"
+        role="alert"
+        v-show="visible"
+        >
+             <i class="rb-alert__icon" :class="[ iconClass, isBigIcon ]" v-if="showIcon"></i>
+            <div class="rb-alert__content">
+                <span class="rb-alert__title" :class="[ isBoldTitle ]" v-if="title || $slots.title">
+                    <slot name="title">{{title}}</slot>
+                </span>
+                <!-- <p></p>
+                <p></p>
+                <p></p> -->
+            </div>
         </div>
     </transition>
 </template>
@@ -17,19 +28,52 @@ export default class RbAlert extends Vue {
         type: String,
         default: ""
     })
-    public title: string;
+    public title: string; //标题
+    @Prop({
+        type: String,
+        default: 'info'
+    })
+    public type:string //主题
+    @Prop({
+        type: Boolean,
+        default:true
+    })
+    public center:Boolean  //文字是否居中
+     @Prop({
+        type: String,
+        default:'light'
+    })
+    public effect:string    //选择提供的主题
+    @Prop({
+        type:Boolean,
+        default:false
+    })
+    public showIcon:boolean //是否显示icon
+    @Prop({
+        type:String,
+        default:''
+    })
+    public description:string //是否显示icon
 
-    get MyName(): string {
-        return `ddd`;
+    public visible:boolean = true
+
+    get typeClass(): string {
+        return `rb-alert--${this.type}`
     }
 
-    // methods
-    sayHello(): void {
-        // alert(`Hello ${this.name}`)
+    get iconClass() :string{
+        return `rb-icon-${this.type}`
     }
 
+    get isBigIcon():string {
+        return this.description || this.$slots.default ? 'is-big' : '';
+    }
+
+    get isBoldTitle():string {
+        return this.description || this.$slots.default ? 'is-bold' : '';
+    }
     mounted() {
-        this.sayHello();
+    
     }
 }
 </script>
