6d83162387af049bd45e09828081571db4f49ec2
任皓 <751135385@qq.com>
2020-04-04 12:11:56 +0800

    [fixed]格式化代码

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import Alert from "../packages/alert/index";
 import "../packages/theme-chalk/src/index.scss";
 const components = [Alert];
 const install = function(Vue, opts = {}) {
-    components.forEach(component => {
-        Vue.component(component.name, component);
-    });
+  components.forEach((component) => {
+    Vue.component(component.name, component);
+  });
 };
 
 // if (typeof window !== 'undefined' && window.Vue) {
@@ -12,7 +12,7 @@ const install = function(Vue, opts = {}) {
 // }
 
 export default {
-    version: "1.0.0",
-    Alert,
-    install
+  version: "1.0.0",
+  Alert,
+  install
 };
