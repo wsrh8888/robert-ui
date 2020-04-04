1d45474b29706801b49a246148ee723c86b11036
任皓 <751135385@qq.com>
2020-04-04 12:10:16 +0800

    [fixed]格式化代码

--- examples/app.vue
+++ examples/app.vue
@@ -1,8 +1,15 @@
 <template>
-    <div>
-        <rb-alert title="dddd" class="ddd" show-icon></rb-alert>
-        <router-view />
+  <div>
+    <div style="width:400px;margin-left:50px;margin-top:20px;">
+      <rb-alert type="warning" show-icon>
+        <template>
+          <div>sfdsfdsfd</div>
+        </template>
+      </rb-alert>
     </div>
+
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,20 +18,25 @@ import Component from "vue-class-component";
 
 @Component
 export default class App extends Vue {
-    // 初始化数据
-    msg = 123;
+  // 初始化数据
+  msg = 123;
+  hello() {
+    alert("Hello World!");
+  }
 }
 </script>
 
 <style lang="css">
- body,html {
-     margin: 0;
-     padding: 0;
-     height: 100%;
-     font-weight: 400;
-     font-family: Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,SimSun,sans-serif;
-     -webkit-font-smoothing: antialiased;
-     -webkit-tap-highlight-color:transparent;
- }
+body,
+html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-weight: 400;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB,
+    Microsoft YaHei, SimSun, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -webkit-tap-highlight-color: transparent;
+}
 </style>
 
--- examples/components/footer.vue
+++ examples/components/footer.vue
@@ -1,17 +1,14 @@
 <template>
-  <div>
-    sdfsdfdsfsds
-  </div>
+  <div>sdfsdfdsfsds</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from "vue";
+import Component from "vue-class-component";
 
-  @Component
-  export default class Footer extends Vue {
-    // 初始化数据
-    msg = 123
-
-  }
+@Component
+export default class Footer extends Vue {
+  // 初始化数据
+  msg = 123;
+}
 </script>
