b5393569949a44d3c0639c27d342ef5b4afe05e6
任皓 <751135385@qq.com>
2020-04-04 12:09:44 +0800

    [fixed]格式化main文件

--- examples/main.ts
+++ examples/main.ts
@@ -2,11 +2,12 @@ import Vue from "vue";
 import entry from "./app.vue";
 import router from "./router";
 import Robert from "../src/index";
-// import "packages/theme-chalk/src/index.scss";
-
+import "packages/theme-chalk/src/index.scss";
+// import Robert from "../lib/robert-ui.common";
+// import "../lib/index.css";
 Vue.use(Robert);
 new Vue({
-    el: "#app",
-    router,
-    render: h => h(entry)
+  el: "#app",
+  router,
+  render: (h) => h(entry)
 });
