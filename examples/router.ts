847d1f6817408022487995b26f89d2025e910b76
任皓 <751135385@qq.com>
2020-04-07 00:27:10 +0800

    [fixed]修改路由文件

--- examples/router.ts
+++ examples/router.ts
@@ -1,15 +1,26 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Footer from './components/footer.vue'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
-
+let userLanguage = window.navigator.language || "en-US";
+let defaultPath = "/en-US";
+if (userLanguage.indexOf("zh-") !== -1) {
+  defaultPath = "/zh-CN";
+} else if (userLanguage.indexOf("es") !== -1) {
+  defaultPath = "/es";
+} else if (userLanguage.indexOf("fr") !== -1) {
+  defaultPath = "/fr-FR";
+}
 export default new Router({
+  mode: "hash",
   routes: [
     {
-      path: '/',
-      name: 'footer',
-      component: Footer
+      path: "/",
+      redirect: defaultPath
+    },
+    {
+      path: "*"
     }
   ]
-})
+});
+
+Vue.use(Router);
